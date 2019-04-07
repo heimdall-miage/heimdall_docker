d9c8685300946262fd19688fe80af3c2d147d745
Sosthèn Gaillard <sosthen.gaillard@gmail.com>
2019-04-07 14:09:57 +0200

    jwt init

--- web/start.sh
+++ web/start.sh
@@ -3,7 +3,17 @@
 if [ ! -d "/home/www/heimdall_web" ]; then
     echo "-- First container startup --"
     git clone https://github.com/heimdall-watch/heimdall_web.git /home/www/heimdall_web
+    mkdir -p /home/www/heimdall_web/config/jwt
+    JWT_PASSPHRASE=`cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1`
+    APP_SECRET=`cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 32 | head -n 1`
+    cat >/home/www/heimdall_web/.env.local <<EOL
+APP_SECRET=${APP_SECRET}
+JWT_PASSPHRASE=${JWT_PASSPHRASE}
+EOL
+    openssl genrsa -out /home/www/heimdall_web/config/jwt/private.pem -aes256 -passout pass:${JWT_PASSPHRASE} 4096
+    openssl rsa -pubout -in /home/www/heimdall_web/config/jwt/private.pem -out /home/www/heimdall_web/config/jwt/public.pem -passin pass:${JWT_PASSPHRASE}
     chown -R heimdall:www-data /home/www/heimdall_web
+    chmod -R 770 /home/www/heimdall_web/config/jwt
 
     if [[ ${APP_ENV} == "dev" ]]; then
         echo "-- Dev env --"
