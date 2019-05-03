77f194864d9c58a14934155c72376a4bd585db62
SosthenG <pro@sosthen.fr>
2019-05-03 12:22:45 +0200

    Yarn cwd

--- web/start.sh
+++ web/start.sh
@@ -20,13 +20,13 @@ EOL
         mv ${PHP_INI_DIR}/php.ini-development ${PHP_INI_DIR}/php.ini
         # To be able to easily modify the project files from the host
         chmod -R 777 /home/www/heimdall_web
-        sudo -u heimdall yarn install
-        sudo -u heimdall yarn encore dev
+        sudo -u heimdall yarn --cwd /home/www/heimdall_web install
+        sudo -u heimdall yarn --cwd /home/www/heimdall_web encore dev
     else
         echo "-- Prod env --"
         mv ${PHP_INI_DIR}/php.ini-production ${PHP_INI_DIR}/php.ini
-        sudo -u heimdall yarn install
-        sudo -u heimdall yarn encore production
+        sudo -u heimdall yarn --cwd /home/www/heimdall_web install
+        sudo -u heimdall yarn --cwd /home/www/heimdall_web encore production
     fi
 
     sudo -u heimdall composer install -d /home/www/heimdall_web
